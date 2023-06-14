720e6515aec0dd963d39a3271e63fd7c454a8383
Karan Jhinga <karan.jhinga@gojek.com>
2023-06-14 16:06:14 +0530

    add fuzzy search endpoint

--- index.js
+++ index.js
@@ -23,6 +23,46 @@ app.get("/cherry/no-order-check/:driverId", (req, res) => {
   });
 });
 
+app.get("/fuzzy_search", (req, res) => {
+  const { q, size } = req.params;
+
+  res.send([
+    {
+      title:
+        "Chained booking cancellation by customer is not reflected on the driver app",
+      subTeam: "Driver FulFilment Transport",
+      issueType: "Chaining/Chained Booking",
+      team: "Driver Platform",
+    },
+    {
+      title: "Wallet linking had failed",
+      subTeam: "Driver Benefits",
+      issueType: "Swadaya",
+      team: "Driver Platform",
+    },
+    {
+      title: "Driver not able to see chained booking marker on map etc",
+      subTeam: "Driver FulFilment Transport",
+      issueType: "Transport Booking Flow issues on Driver App",
+      team: "Driver Platform",
+    },
+    {
+      title:
+        "My driver is not showing as top driver in the customer booking page",
+      subTeam: "Driver Quality",
+      issueType: "DPS or Driver Performance Score",
+      team: "Driver Platform",
+    },
+    {
+      title:
+        "Issues related to booking flow Including chained/back to back bookings, multi stop booking",
+      subTeam: "Driver FulFilment Transport",
+      issueType: "Transport Booking Flow issues on Driver App",
+      team: "Driver Platform",
+    },
+  ]);
+});
+
 // Start the server
 const port = 3000;
 app.listen(port, () => {
